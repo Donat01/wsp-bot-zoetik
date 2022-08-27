f3fce624b0660bfa37ebf58eadc43630e9a398b1
Donat01k <edisondonato303@gmail.com>
2022-08-27 03:38:46 -0500

    Updated

---
+++ app.js
@@ -0,0 +1,36 @@
+const venom = require('venom-bot');
+
+try {
+
+    venom
+    .create({
+      session: 'session-name', //name of session
+      multidevice: true // for version not multidevice use false.(default: true)
+    })
+    .then((client) => start(client))
+    .catch((erro) => {
+      console.log(erro);
+    });
+  
+  function start(client) {
+    client.onMessage((message) => {
+      if (message.body === 'Hi' && message.isGroupMsg === false) {
+        client
+          .sendText(message.from, 'Welcome Venom 🕷')
+          .then((result) => {
+            console.log('Result: ', result); //return object success
+          })
+          .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
+      }
+    });
+  }
+  
+  process.on('SIGINT', function() {
+      client.close();
+    });
+
+ } catch (error) {
+    client.close();
+ }
