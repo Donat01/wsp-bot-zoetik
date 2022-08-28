f6d24bf931bd4102e394a7599c95d917bb85dd18
Donat01k <edisondonato303@gmail.com>
2022-08-28 00:25:40 -0500

    Updated

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const qrc = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+
+const client = new Client({ puppeteer: { headless: true }, clientId: 'project' });
+client.initialize();
+
+client.on('qr', (qr) => {
+    // NOTE: This event will not be fired if a session is specified.
+    qrc.generate(qr, {small: true});
+});
+
+client.on('authenticated', (sessions) => {
+    console.log('AUTHENTICATED', sessions);
+});
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessfull
+    console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('ready', () => {
+    console.log('READY');
+});
+
+client.on('message', async msg => {
+    console.log('MESSAGE RECEIVED', msg);
+});
+
+client.on('message_create', (msg) => {
+    // Fired on all message creations, including your own
+    if (msg.fromMe) {
+        console.log(msg)
+    }
+});
